9b6ad318180fb1bec28b5c121df5eb04853cd88a
cjredmond <connor.redmond@gmail.com>
2016-09-13 14:42:24 -0400

    Starting hard mode and finishing main

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ while count < 5:
     count = count + 1
     print("You used %o of 5 guesses" % count)
     if count < 5:
-        guess = int(input("What is your guess? (between 1 and 100)"))
+        guess = int(input("What is your guess? (between 1 and 100)    "))
     else:
         print("Sorry, you lose")
     if guess == answer:
-        print("YOU WIN")
+        return("YOU WIN")
     elif guess < (answer + 3) and guess > (answer - 3):
         print("You are within 3 numbers of the answer")
     elif guess > answer:
