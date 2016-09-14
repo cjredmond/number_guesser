ebe12a8b59527f2e42f5912116f7ba3d1b47a13c
cjredmond <connor.redmond@gmail.com>
2016-09-13 22:11:28 -0400

    finished very hard mode

--- very_hard_guesser.py
+++ very_hard_guesser.py
@@ -4,16 +4,26 @@ while answer >= 101:
     print("You need to chose a number between 1 and 100")
     answer = int(input("Pick a number between 1 and 100:    "))
 
-print("good number")
+print("You chose a valid number")
 count = -1
-win = False
+min_guess = 1
+max_guess = 100
+
 while count < 7:
-    count = count + 1
-    comp_guess = random.randint(1,100)
+
+    comp_guess = int((min_guess + max_guess) / 2)
     print(comp_guess)
+
+
     if comp_guess == answer:
         print("Computer Wins!")
+        break
     elif comp_guess < answer:
         print("Too Low")
+        min_guess = (comp_guess + 1)
+        count = count + 1
     else:
+        print(comp_guess)
         print("Too High")
+        max_guess = (comp_guess -1)
+        count = count + 1
